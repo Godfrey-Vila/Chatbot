555ef31e73e96f4678033d12eb3ccbf6c52749e2
Godfrey-Vila <vila.godfreyc@gmail.com>
2022-11-19 17:32:49 +0800

    testing and troubleshooting

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,5 @@
 import re
-import Responses as long
+import long_responses as long
 
 print("********** PROGRAMMED BY:*************")
 print("********** GODFREY VILA **************")
@@ -14,17 +14,20 @@ def message_probability(user_message, recognised_words, single_response=False, r
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
+
     # calculate the percentage of the recognized words in user.
     percentage = float(message_certainty) / float(len(recognised_words))
 
-    for word in recognised_words:
+    for word in required_words:
         if word not in user_message:
             has_required_words = False
+            break
 
     if has_required_words or single_response:
         return int(percentage*100)
     else:
         return 0
+
 def check_all_message(message):
     highest_prob_list = {}
 
@@ -39,8 +42,13 @@ def check_all_message(message):
     response('I\'m Great!!, How about you?',['how', 'are', 'you'], required_words=['how'])
     response('Thank You Very much', ['i', 'love', 'you', 'vision'], required_words=['love', 'vision'])
 
-def get_response(input):
-    split_message = re.split(r'\s+|[,;?!.-]\s*', input.lower())
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    print(highest_prob_list)
+
+    return best_match
+
+def get_response(user_input):
+    split_message = re.split(r'\s|[,;?!.-]\s*', user_input.lower())
     response = check_all_message(split_message)
     return response
 #testing response
