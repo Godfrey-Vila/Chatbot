0b6af484b24f1cc7e455d3ee8eef0dd0c11e272d
Godfrey-Vila <vila.godfreyc@gmail.com>
2022-11-19 16:08:12 +0800

    import and name

---
+++ Chatbot.py
@@ -0,0 +1,9 @@
+import re
+import Responses as long
+
+print("********** PROGRAMMED BY:*************")
+print("********** GODFREY VILA **************")
+print("******** SECTION BSCOE 2-2 ***********")
+print("******* Sir Danilo Madrigalejos ******")
+print("")
+
