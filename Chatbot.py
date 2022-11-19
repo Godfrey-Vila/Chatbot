fa16030f0ff205c2f67bc44888f5778ecc7c14ab
Godfrey-Vila <vila.godfreyc@gmail.com>
2022-11-19 16:38:07 +0800

    message probability

--- Chatbot.py
+++ Chatbot.py
@@ -7,6 +7,10 @@ print("******** SECTION BSCOE 2-2 ***********")
 print("******* Sir Danilo Madrigalejos ******")
 print("")
 
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
 def get_response(input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', input.lower())
     response = check_all_message(split_message)
